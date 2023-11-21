b3b5a55427f02ec18ec6ec551018e7fb6ec8552c
Sean-donny <deanpatrick007@gmail.com>
2023-11-21 21:23:51 +0000

    Fixed Navbar structure

--- resume-site/src/components/Navbar.tsx
+++ resume-site/src/components/Navbar.tsx
@@ -2,21 +2,27 @@ import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between w-full fixed px-5 py-2">
-      <motion.p
-        className="font-custom font-semibold text-aquatic tracking-tight cursor-crosshair"
-        whileHover={{ scale: 1.2618 }}
-        whileTap={{ scale: 0.9 }}
-      >
-        Sean Donny
-      </motion.p>
-      <motion.button
-        className="font-custom font-semibold text-aquatic tracking-tight cursor-crosshair"
-        whileHover={{ scale: 1.2618 }}
-        whileTap={{ scale: 0.9 }}
-      >
-        Menu
-      </motion.button>
+    <nav className="w-full fixed px-5 py-2 z-10">
+      <ul className="flex justify-between">
+        <li>
+          <motion.h1
+            className="font-custom font-semibold text-aquatic tracking-tight cursor-crosshair"
+            whileHover={{ scale: 1.2618 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Sean Donny
+          </motion.h1>
+        </li>
+        <li>
+          <motion.button
+            className="font-custom font-semibold text-aquatic tracking-tight cursor-crosshair"
+            whileHover={{ scale: 1.2618 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Menu
+          </motion.button>
+        </li>
+      </ul>
     </nav>
   );
 };

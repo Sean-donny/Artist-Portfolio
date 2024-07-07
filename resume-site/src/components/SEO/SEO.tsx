5fe5fdd8c46bbe3783effa0a64190ebc8a9675d9
Sean-donny <deanpatrick007@gmail.com>
2024-07-07 22:02:15 +0100

    updates sitemap + changed default URL

--- resume-site/src/components/SEO/SEO.tsx
+++ resume-site/src/components/SEO/SEO.tsx
@@ -11,34 +11,37 @@ type SEOProps = {
 export default function SEO({
   title,
   description,
-  url,
-  image = '',
+  url = 'https://seandonny.com', // Default URL
+  image = '', // Default to empty if no image provided
   type = 'website',
 }: SEOProps) {
   return (
     <Helmet>
       {/* Standard metadata tags */}
-      <title>{title} | Sean Donny</title>
+      <title>{`${title} | Sean Donny`}</title>
       <meta name="description" content={description} />
       <meta name="author" content="Sean Donny" />
       <link rel="canonical" href={url} />
       {/* End standard metadata tags */}
+
       {/* Facebook tags */}
       <meta property="og:type" content={type} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
-      <meta property="og:image" content={`https://www.seandonny.com${image}`} />
+      {image && (
+        <meta property="og:image" content={`https://seandonny.com${image}`} />
+      )}
       {/* End Facebook tags */}
+
       {/* Twitter tags */}
       <meta name="twitter:creator" content="@blvvvckfire" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta
-        name="twitter:image"
-        content={`https://www.seandonny.com${image}`}
-      />
+      {image && (
+        <meta name="twitter:image" content={`https://seandonny.com${image}`} />
+      )}
       {/* End Twitter tags */}
     </Helmet>
   );
